1873294b596a7d18387fd282db42bd62fb260775
amuname <74045707+amuname@users.noreply.github.com>
2020-11-27 17:48:14 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,17 +1,18 @@
-var Msg;
-var senderNumber;
-var CrmAPI;
-var idForPost;
-var url = "https://app.syncrm.ru/api/v1/contacts/";
-// var filter = "filter[any_phone]=+";
-var fulFilter;
-var header1name = "Content-Type";
-var header1val = "application/vnd.api+json"
-var header2name = "Authorization";
-var header2val;
-var data;
-var xhr = new XMLHttpRequest;
-var xhrPatch =new XMLHttpRequest;
+var Msg,
+senderNumber,
+CrmAPI,
+idForPost,
+url = "https://app.syncrm.ru/api/v1/contacts",
+filter = "?filter%5Bgeneral_phone%5D=%2B",
+fulFilter,
+header1name = "Content-Type",
+header1val = "application/vnd.api+json",
+header2name = "Authorization",
+header2val,
+data,
+xhr = new XMLHttpRequest,
+xhrPatch =new XMLHttpRequest,
+xhrFil =new XMLHttpRequest;
 console.log(Msg);
 console.log(senderNumber);
 // 	console.log("wow")
@@ -40,16 +41,29 @@ function handleMessage(request, sender, sendResponse) {
      //      			console.log('Value currently is ' + result.ContactID);
      //      			idForPost = result.ContactID;
      //    			});
-	         	//    fulFilter = encodeURI/*Component*/(filter+senderNumber);
+	         	fulFilter = filter+senderNumber;
 	         	//    console.log(fulFilter);
 	        	header2val= "Bearer "+CrmAPI;
 	        	
 	        	// console.log(CrmAPI);
-	        		getInfoCrm();
+	        		filterBool(argument);
+	        		
 	        	 // }
 			}
         });
 
+}
+
+function filterBool(argument) {
+	xhrFil.open("GET", url+fulFilter)
+	xhrFil.setRequestHeader(header1name,header1val);
+	xhrFil.setRequestHeader(header2name,header2val);
+	xhrFil.send();
+	xhrFil.onload = ()=> {
+		let r = JSON.parse(xhrFil.response);
+		if (r.data.length!==0) {}
+	}
+	getInfoCrm();
 }
 
 function getInfoCrm() {
@@ -65,7 +79,7 @@ function getInfoCrm() {
 			}
 		}
 	});
-	xhr.open("GET",url+idForPost);
+	xhr.open("GET",url+"/"+idForPost);
 	xhr.setRequestHeader(header1name,header1val);
 	xhr.setRequestHeader(header2name,header2val);
 	xhr.send();
@@ -74,17 +88,17 @@ function getInfoCrm() {
 		//console.log(xhr);
 		var r = JSON.parse(xhr.response);
 		//console.log(r);
-		console.log(r.data.attributes.description);
+		//console.log(r.data.attributes.description);
 		description = r.data.attributes.description;
 		if (description==null||description=="") {
-			xhrPatch.open("PATCH",url+idForPost);
+			xhrPatch.open("PATCH",url+"/"+idForPost);
 			xhrPatch.setRequestHeader(header1name,header1val);
 			xhrPatch.setRequestHeader(header2name,header2val);
 			xhrPatch.send(data);
 		}else {
 			
 			var oldMsg =description.slice(-Msg.length);
-			console.log(oldMsg);
+			//console.log(oldMsg);
 			if (oldMsg!==Msg) {
 				newMsg = description+" <p>"+Msg;
 				data = JSON.stringify({
@@ -96,7 +110,7 @@ function getInfoCrm() {
 				      }
 				   }
 				});
-			xhrPatch.open("PATCH",url+idForPost);
+			xhrPatch.open("PATCH",url+"/"+idForPost);
 			xhrPatch.setRequestHeader(header1name,header1val);
 			xhrPatch.setRequestHeader(header2name,header2val);
 			xhrPatch.send(data);
@@ -116,12 +130,12 @@ function getInfoCrm() {
 // }
 
 chrome.storage.sync.get(["APl"], function(result) {
-          console.log('Value currently is ' + result.APl);
+          //console.log('Value currently is ' + result.APl);
           CrmAPI = result.APl;
         });
 
 chrome.storage.sync.get(["ContactID"], function(result) {
-          console.log('Value currently is ' + result.ContactID);
+          //console.log('Value currently is ' + result.ContactID);
           idForPost = result.ContactID;
         });
 
