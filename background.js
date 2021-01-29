462e8a60a349f4004dbc29f6c3b222470a8ebc49
alex mesh <3steps2die@gmail.com>
2021-01-29 18:07:47 +0300

    Update background.js

--- background.js
+++ background.js
@@ -17,111 +17,120 @@ xhrPatch =new XMLHttpRequest,
 xhrFil =new XMLHttpRequest;
 
 function handleMessage(request, sender, sendResponse) {
-	if (lengthmsg=="") {
-		lengthmsg = request;
-		console.log(lengthmsg);
-	} else {
-			console.log("another one")
-			Msg = request.slice(0,-11);
-	    	senderNumber = request.slice(-11);
-	        chrome.storage.sync.get(["APl"], function(result) {
-	          	//console.log('Value currently is ' + result.APl);
-	          	CrmAPI = result.APl;
-	            if (CrmAPI!==undefined) {
-	         	fulFilter = filter+senderNumber;
-	        	header2val= "Bearer "+CrmAPI;
-	        	filterBool(lengthmsg).then(toProm);
-	        }
-        });
+	console.log(request);
+	console.log(sender);
+	if (sender.origin =='https://app.syncrm.ru') {
+		console.log(sender.tab.id)
+		sendResponse = ()=>{
+
+			chrome.tabs.sendMessage(sender.tab.id,{a:true});
+		}
 	}
-}
+// 	if (lengthmsg=="") {
+// 		lengthmsg = request;
+// 		console.log(lengthmsg);
+// 	} else {
+// 			console.log("another one")
+// 			Msg = request.slice(0,-11);
+// 	    	senderNumber = request.slice(-11);
+// 	        chrome.storage.sync.get(["APl"], function(result) {
+// 	          	//console.log('Value currently is ' + result.APl);
+// 	          	CrmAPI = result.APl;
+// 	            if (CrmAPI!==undefined) {
+// 	         	fulFilter = filter+senderNumber;
+// 	        	header2val= "Bearer "+CrmAPI;
+// 	        	filterBool(lengthmsg).then(toProm);
+// 	        }
+//         });
+// 	}
+// }
 
-function filterBool(msgLength) {
-	msgLength=parseInt(msgLength)+8;
-	console.log(msgLength);
-	return new Promise(function(resolve,reject){
-		xhrFil.open("GET", url+fulFilter)
-		xhrFil.setRequestHeader(header1name,header1val);
-		xhrFil.setRequestHeader(header2name,header2val);
-		xhrFil.onload = ()=> {
-			let argToProm = JSON.parse(xhrFil.response)
-			resolve({argToProm,msgLength});
-		}
-		xhrFil.send();
-	})
-}
+// function filterBool(msgLength) {
+// 	msgLength=parseInt(msgLength)+8;
+// 	console.log(msgLength);
+// 	return new Promise(function(resolve,reject){
+// 		xhrFil.open("GET", url+fulFilter)
+// 		xhrFil.setRequestHeader(header1name,header1val);
+// 		xhrFil.setRequestHeader(header2name,header2val);
+// 		xhrFil.onload = ()=> {
+// 			let argToProm = JSON.parse(xhrFil.response)
+// 			resolve({argToProm,msgLength});
+// 		}
+// 		xhrFil.send();
+// 	})
+// }
 
-function toProm(r){
-	console.log(r.argToProm);
-	console.log(r.msgLength);
-	console.log(r.argToProm.data.length);
-	for (let i =  r.argToProm.data.length - 1; i >= 0; i--) {
-		let d =r.argToProm.data[i];
-		console.log(`GONA Send to id ${d.id} next`);
-		description = d.attributes.description;
-		console.log(description);
-		idForPost = d.id;
-		getInfoCrm(r.msgLength);
-	}
-}
+// function toProm(r){
+// 	console.log(r.argToProm);
+// 	console.log(r.msgLength);
+// 	console.log(r.argToProm.data.length);
+// 	for (let i =  r.argToProm.data.length - 1; i >= 0; i--) {
+// 		let d =r.argToProm.data[i];
+// 		console.log(`GONA Send to id ${d.id} next`);
+// 		description = d.attributes.description;
+// 		console.log(description);
+// 		idForPost = d.id;
+// 		getInfoCrm(r.msgLength);
+// 	}
+// }
 
-function getInfoCrm(realLength) {
-	lengthmsg="";
-    	data = JSON.stringify({
-			"data": {
-			"type":"contacts",
-			"id":idForPost,
-				"attributes":{
-				"description": Msg
-			}
-		}
-	});
+// function getInfoCrm(realLength) {
+// 	lengthmsg="";
+//     	data = JSON.stringify({
+// 			"data": {
+// 			"type":"contacts",
+// 			"id":idForPost,
+// 				"attributes":{
+// 				"description": Msg
+// 			}
+// 		}
+// 	});
 
-		if (description===null&&typeof description===('object')||description=="") {
-			xhrPatch.open("PATCH",url+"/"+idForPost, false);
-			xhrPatch.setRequestHeader(header1name,header1val);
-			xhrPatch.setRequestHeader(header2name,header2val);
-			xhrPatch.send(data);
-		}else if (description.length<=Msg.length) {
-			let newMsg= description+Msg.slice(-realLength);
-			console.log(newMsg);
-			data = JSON.stringify({
-				  "data": {
-					  "type":"contacts",
-					  "id":idForPost,
-					  "attributes":{
-					    "description": newMsg
-				      }
-				   }
-				});
-			xhrPatch.open("PATCH",url+"/"+idForPost, false);
-			xhrPatch.setRequestHeader(header1name,header1val);
-			xhrPatch.setRequestHeader(header2name,header2val);
-			xhrPatch.onload = ()=>{
-				console.log(JSON.parse(xhrPatch.response));
-			}
-			xhrPatch.send(data);
-		} else {
-			var oldMsg =Msg.slice(description.length);
-			if (oldMsg!==Msg) {
-				let newMsg = description+ Msg.slice(-description.length);
-				console.log(newMsg);
-				data = JSON.stringify({
-				  "data": {
-					  "type":"contacts",
-					  "id":idForPost,
-					  "attributes":{
-					    "description": newMsg
-				      }
-				   }
-				});
-			xhrPatch.open("PATCH",url+"/"+idForPost, false);
-			xhrPatch.setRequestHeader(header1name,header1val);
-			xhrPatch.setRequestHeader(header2name,header2val);
-			xhrPatch.send(data);
-			}
+// 		if (description===null&&typeof description===('object')||description=="") {
+// 			xhrPatch.open("PATCH",url+"/"+idForPost, false);
+// 			xhrPatch.setRequestHeader(header1name,header1val);
+// 			xhrPatch.setRequestHeader(header2name,header2val);
+// 			xhrPatch.send(data);
+// 		}else if (description.length<=Msg.length) {
+// 			let newMsg= description+Msg.slice(-realLength);
+// 			console.log(newMsg);
+// 			data = JSON.stringify({
+// 				  "data": {
+// 					  "type":"contacts",
+// 					  "id":idForPost,
+// 					  "attributes":{
+// 					    "description": newMsg
+// 				      }
+// 				   }
+// 				});
+// 			xhrPatch.open("PATCH",url+"/"+idForPost, false);
+// 			xhrPatch.setRequestHeader(header1name,header1val);
+// 			xhrPatch.setRequestHeader(header2name,header2val);
+// 			xhrPatch.onload = ()=>{
+// 				console.log(JSON.parse(xhrPatch.response));
+// 			}
+// 			xhrPatch.send(data);
+// 		} else {
+// 			var oldMsg =Msg.slice(description.length);
+// 			if (oldMsg!==Msg) {
+// 				let newMsg = description+ Msg.slice(-description.length);
+// 				console.log(newMsg);
+// 				data = JSON.stringify({
+// 				  "data": {
+// 					  "type":"contacts",
+// 					  "id":idForPost,
+// 					  "attributes":{
+// 					    "description": newMsg
+// 				      }
+// 				   }
+// 				});
+// 			xhrPatch.open("PATCH",url+"/"+idForPost, false);
+// 			xhrPatch.setRequestHeader(header1name,header1val);
+// 			xhrPatch.setRequestHeader(header2name,header2val);
+// 			xhrPatch.send(data);
+// 			}
 
-		}
+// 		}
 
 }
 
@@ -137,3 +146,4 @@ chrome.storage.sync.get(["ContactID"], function(result) {
         });
 
 chrome.runtime.onMessage.addListener(handleMessage);
+
